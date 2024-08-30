08204be6e08d7b346e5b8f09c4ec98af9047fc7c
yihua <pao.wen25@gmail.com>
2024-08-30 09:28:57 +0800

    flask_practice

---
+++ VuePlusFlask/crud/client/src/components/Ping.vue
@@ -0,0 +1,36 @@
+<template>
+    <div>
+      <p>{{ msg }}</p>
+    </div>
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  
+  export default {
+    name: 'Ping',
+    data() {
+      return {
+        msg: '',
+      };
+    },
+    methods: {
+      getMessage() {
+        const path = 'http://localhost:5001/ping';
+        fetch(path)
+          .then((res) => {
+            return res.json();
+          }).then((res_json)=>{
+            this.msg = res_json;
+          })
+          .catch((error) => {
+            
+            console.error(error);
+          });
+      },
+    },
+    created() {
+      this.getMessage();
+    },
+  };
+  </script>
